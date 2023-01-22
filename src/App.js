b7e89f9bb090a4ff1317df274f3d2a2bd4b10ad6
emmyjay387 <104701190+emmyjay387@users.noreply.github.com>
2023-01-23 07:57:07 +1300

    edit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import ResetPassword from './components/login/ResetPassword';
 import Customer from './components/dashboard/Customer';
 
 const App = () => {
+  console.log(process.env);
   return (
     <StateProvider>
     <Router>
