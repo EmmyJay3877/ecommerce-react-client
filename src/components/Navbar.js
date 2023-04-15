667de154c6b4c311c66f59c63bceddb0afcd79ee
emmyjay387 <104701190+emmyjay387@users.noreply.github.com>
2023-04-16 03:21:10 +1300

    edit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiMenu } from 'react-icons/fi'
 
 const Navbar = () => {
 
+  const [showMenu, setShowMenu] = useState(false)
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
   return (
 <div className="">
-	<nav className="sm:flex sm:justify-between md:flex-row md:justify-between relative px-4 py-4 lg:flex lg:justify-between lg:items-center bg-white ">
+	<nav className="flex justify-between items-center md:flex-row md:justify-between relative px-4 py-4 lg:flex lg:justify-between lg:items-center bg-white">
     <Link to={'/'}>
-		<h1 className="lg:text-3xl lg:font-bold lg:leading-none md:text-2xl md:font-semibold sm:text-2xl sm:font-semibold">
+		<h1 className="lg:text-3xl lg:font-bold lg:leading-none md:text-2xl md:font-semibold text-2xl font-semibold">
 		Dynamic Headphones
 		</h1>
     </Link>
@@ -27,7 +33,26 @@ const Navbar = () => {
     >Log In
     </button>
     </Link>
-    <div className='lg:hidden md:hidden'><FiMenu size={40}/></div>
+    </div>
+    <div className='lg:hidden md:hidden mx-4'>
+    <div className=' cursor-pointer' onClick={toggleMenu}><FiMenu size={40}/></div>
+    {showMenu && (
+        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+        <hr className="border-gray-200 dark:border-gray-700 "/>
+          <div className="py-1" role="none">
+           <Link to={'/login'}>
+            <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left" role="menuitem">
+              Login
+            </button>
+           </Link>
+           <Link to={'/signup'}>
+            <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left" role="menuitem">
+              SignUp
+            </button>
+           </Link>
+          </div>
+        </div>
+      )}
     </div>
 	</nav>
 </div>
