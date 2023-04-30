879a76040d4d464e456b152fa894d11df0d2a086
emmyjay387 <104701190+emmyjay387@users.noreply.github.com>
2023-04-30 11:59:25 +0100

    edit

--- src/components/Failed.js
+++ src/components/Failed.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { BiError } from 'react-icons/bi'
 
 const Failed = () => {
   return (
-    <div>Failed</div>
+    <div className="success-wrapper">
+    <div className="success">
+      <p className="icon">
+        <BiError />
+      </p>
+      <h2>Oops, your payment failed</h2>
+      <p className="email-msg">Please return to your dashboard and retry.</p>
+      <p className="description">
+        If you have any questions, please email
+        <a className="email" href="mailto:order@example.com">
+          dynamicheadphones@gmail.com
+        </a>
+      </p>
+      <Link to={'/customer/'}>
+        <button type="button" width="300px" className="btn">
+          Go back to dashboard
+        </button>
+      </Link>
+    </div>
+  </div>
   )
 }
 
