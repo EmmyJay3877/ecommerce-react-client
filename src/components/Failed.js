30b98e4aa8a8b865485d37778abcc4372df0d58a
emmyjay387 <104701190+emmyjay387@users.noreply.github.com>
2023-08-15 12:53:03 +0100

    component edit

--- src/components/Failed.js
+++ src/components/Failed.js
@@ -1,28 +1,29 @@
 import React from 'react'
 import { BiError } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
 
 const Failed = () => {
   return (
     <div className="success-wrapper">
-    <div className="success">
-      <p className="icon">
-        <BiError />
-      </p>
-      <h2>Oops, your payment failed</h2>
-      <p className="email-msg">Please return to your dashboard and retry.</p>
-      <p className="description">
-        If you have any questions, please email
-        <a className="email" href="mailto:order@example.com">
-          dynamicheadphones@gmail.com
-        </a>
-      </p>
-      <Link to={'/customer/'}>
-        <button type="button" width="300px" className="btn">
-          Go back to dashboard
-        </button>
-      </Link>
+      <div className="success">
+        <p className="icon">
+          <BiError />
+        </p>
+        <h2>Oops, your payment failed</h2>
+        <p className="email-msg">Please return to your dashboard and retry.</p>
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:order@example.com">
+            dynamicheadphones@gmail.com
+          </a>
+        </p>
+        <Link to={'/customer/'}>
+          <button type="button" width="300px" className="btn">
+            Go back to dashboard
+          </button>
+        </Link>
+      </div>
     </div>
-  </div>
   )
 }
 
